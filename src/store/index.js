3f7b92868f85bfd3a60037a2df0b105c313472c6
marek <kursygitarowekopernik@gmail.com>
2018-02-28 13:32:45 +0100

    dodane devtools

---
+++ src/store/index.js
@@ -0,0 +1,10 @@
+import reducers from '../reducers/index';
+import { createStore, combineReducers } from 'redux';
+import DevTools from './DevTools';
+
+const store = createStore(
+  reducers,
+  DevTools.instrument()
+);
+
+export default store;
